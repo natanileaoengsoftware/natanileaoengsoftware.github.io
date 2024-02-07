d4ac9753304036f624140c081473b6c5263204c2
Natani Germann Selau Leão <natanigselau@gmail.com>
2024-02-07 09:55:34 -0300

    adicionando info

--- js.js
+++ js.js
@@ -1,13 +1,11 @@
 function abrirWhatsapp() {
   var nome = document.getElementById("nome").value;
-  var telefone = document.getElementById("telefone").value;
   var email = document.getElementById("email").value;
   var msg = document.getElementById("msg").value;
   var url = "https://wa.me/5551991260929?text=" // Seu numero
     + "Seu Site Personalizado" + "%0a" // Mensagem personalizada
     + "%0a" // Quebra de linha
     + "*Nome*: " + nome + "%0a" // Dados do formulário
-    + "*Telefone*: " + telefone + "%0a"
     + "*E-mail*: " + email + "%0a"
     + "*Mensagem*: " + msg;
   window.open(url, '_blank').focus();
